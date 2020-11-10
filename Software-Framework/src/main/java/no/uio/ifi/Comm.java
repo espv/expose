f587a8de396e36ec3b8b4cd382254eb61285ca1c
Espen Volnes <espen.volnes@gmail.com>
2020-11-10 12:00:43 +0100

    removed unused printouts

--- Software-Framework/src/main/java/no/uio/ifi/Comm.java
+++ Software-Framework/src/main/java/no/uio/ifi/Comm.java
@@ -13,11 +13,8 @@ public class Comm {
 
     static Map<String, Object> receiveMap(BufferedReader reader, Yaml yaml) throws IOException {
         String received = reader.readLine();
-        System.out.println("Received map " + received);
         String yaml_event = StringEscapeUtils.unescapeJava(received);
-        System.out.println("Received yaml event " + yaml_event);
         Map<String, Object> map;
-        System.out.println("Before loading yaml");
         synchronized (yaml) {
             map = yaml.load(yaml_event);
         }
@@ -26,14 +23,11 @@ public class Comm {
     }
 
     public void SendMap(Map<String, Object> map, PrintWriter writer) {
-        System.out.println("Sending map " + map);
         String raw_yaml;
         synchronized (yaml) {
             raw_yaml = yaml.dump(map);
         }
-        System.out.println("Sending raw_yaml " + raw_yaml);
         String yaml_event = StringEscapeUtils.escapeJava(raw_yaml) + "\n";
-        System.out.println("Sending yaml_event " + yaml_event);
         writer.print(yaml_event);
         writer.flush();
     }
