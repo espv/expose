bf370f75a306c93a3a81205ece88931b567e1129
Espen Volnes <espenvol@ifi.uio.no>
2021-07-01 12:24:05 +0200

    fixed experimentframework jar name

--- Software-Framework/add-to-local-maven-repo.sh
+++ Software-Framework/add-to-local-maven-repo.sh
@@ -2,7 +2,7 @@
 # Use to place Expose as a local maven module which SPE wrappers can use as dependency
 mvn clean package && \
 	mvn install:install-file \
-	-Dfile=target/StreamProcessingExperimentFramework-1.0.0.jar \
+	-Dfile=target/experimentframework-1.0.jar \
 	-DgroupId=no.uio.ifi \
 	-DartifactId=experimentframework \
 	-Dversion=1.0 \
